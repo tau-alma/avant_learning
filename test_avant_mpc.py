cfbb9e34cbf0c9fb61fdb1ca1396380232a53aed
Aleksi Mäki-Penttilä <aleksi.maki-penttila@tuni.fi>
2024-08-12 22:25:11 +0300

    before adding input delays to MPC

--- test_avant_mpc.py
+++ test_avant_mpc.py
@@ -8,7 +8,6 @@ from argparse import ArgumentParser
 from sparse_to_dense_reward.avant_goal_env import AvantGoalEnv
 from mpc_solvers.mpc_problem import SymbolicMPCProblem
 from mpc_solvers.casadi_collocation_solver import CasadiCollocationSolver
-from mpc_solvers.casadi_shooting_solver import CasadiShootingSolver
 from mpc_solvers.acados_solver import AcadosSolver
 from utils import MLP
 
@@ -24,7 +23,10 @@ class MPCActor:
         theta_f = cs.MX.sym("theta_f")
         beta = cs.MX.sym("beta")
         dot_beta_ref = cs.MX.sym("dot_beta_ref")
+        dot_beta_ref_del2 = cs.MX.sym("dot_beta_ref_del2")
+        dot_beta_ref_del1 = cs.MX.sym("dot_beta_ref_del1")
         v_f_ref = cs.MX.sym("v_f_ref")
+        v_f_ref_del1 = cs.MX.sym("v_f_ref_del1")
 
         ocp_x = cs.vertcat(x_f, y_f, theta_f, beta, dot_beta_ref, v_f_ref)
         lbx_vec = np.array([
@@ -72,13 +74,12 @@ class MPCActor:
         f = cs.Function('f', [ocp_x, ocp_u, ocp_p], [f_expr])
 
         accel_penalty = 1e-3*dot_dot_beta**2 + 1e-3*a_f**2
-        policy_deviation = (dot_beta_ref - policy_dot_beta)**2 + (v_f_ref - policy_v_f)**2
         standstill_turning_penalty = (1 - cs.tanh(v_f_ref**2 / 0.15)) * 10*(dot_beta_ref)**2
         l = cs.Function('l', [ocp_x, ocp_u, ocp_p], [accel_penalty + standstill_turning_penalty])
 
         problem = SymbolicMPCProblem(
             N=10,
-            h=0.2,
+            h=0.1,
             ocp_x=ocp_x,
             lbx_vec=lbx_vec,
             ubx_vec=ubx_vec,
@@ -92,31 +93,23 @@ class MPCActor:
         )
  
         # Terminal cost with Q network:
-        critic_state = cs.vertcat(
+        critic_stage_state = cs.vertcat(
             x_goal - x_f,       y_goal - y_f,    
             cs.sin(theta_f),    cs.cos(theta_f), 
             cs.sin(theta_goal), cs.cos(theta_goal), 
             beta, dot_beta_ref, v_f_ref, 
-            dot_beta_ref,       v_f_ref
+            dot_dot_beta / config.avant_max_dot_dot_beta, a_f / config.avant_max_a
+        )
+        critic_terminal_state = cs.vertcat(
+            x_goal - x_f,       y_goal - y_f,    
+            cs.sin(theta_f),    cs.cos(theta_f), 
+            cs.sin(theta_goal), cs.cos(theta_goal), 
+            beta, dot_beta_ref, v_f_ref, 
+            0, 0
         )
         critic_model = torch.load("avant_critic").eval()
-        problem.add_stage_neural_cost(model=critic_model, model_state=critic_state)
-        problem.add_terminal_neural_cost(model=critic_model, model_state=critic_state)
-
-        # Reference targets with policy:
-        policy_model = MLP(9, [18, 36, 72, 36, 18], 2).eval()
-        policy_model.load_state_dict(torch.load("avant_actor"))
-        actor_state = critic_state[:-2]
-        sim_x = cs.vertcat(x_f, y_f, theta_f, beta)
-        sim_u = cs.vertcat(dot_beta_ref, v_f_ref)
-        sim_f_expr = cs.vertcat(
-            v_f_ref * cs.cos(theta_f),
-            v_f_ref * cs.sin(theta_f),
-            omega_f,
-            dot_beta_ref
-        )
-        f_s = cs.Function("sim_fun", [sim_x, sim_u], [sim_f_expr])
-        # problem.add_neural_policy(model=policy_model, model_state=actor_state, sim_fun=f_s)
+        problem.add_stage_neural_cost(model=critic_model, model_state=critic_stage_state)
+        problem.add_terminal_neural_cost(model=critic_model, model_state=critic_terminal_state)
 
         self.solver = solver_class(problem, rebuild=True)
 
@@ -152,70 +145,19 @@ class MPCActor:
 
         return controls, sol_x[2:, :4]
     
-class RLActor:
-    def __init__(self):
-        self.network = torch.load("avant_actor_full").eval()
-        self.beta_vel = 0
-        self.vel_f = 0
-
-    def reset(self):
-        self.beta_vel = 0
-        self.vel_f = 0
-
-    def act(self, observation) -> np.ndarray:
-        torch_state = {k: torch.from_numpy(v).to(self.network.device) for k, v in observation.items()}
-        with torch.no_grad():
-            policy_outputs = self.network(torch_state)[0]
-        policy_outputs = policy_outputs.cpu().numpy()
-
-        # Rate limit the change of beta velocity target command by max allowed acceleration:
-        new_beta_vel = max(
-            min(
-                policy_outputs[0] * config.avant_max_dot_beta,
-                self.beta_vel + 0.1 * config.avant_max_dot_dot_beta
-            ),
-            self.beta_vel - 0.1 * config.avant_max_dot_dot_beta
-        )
-        self.beta_vel = new_beta_vel
-
-        # Rate limit the change of linear velocity target command by max allowed acceleration:
-        self.vel_f = max(
-            min(
-                policy_outputs[1] * config.avant_max_v,
-                self.vel_f + 0.1 * config.avant_max_a
-            ),
-            self.vel_f - 0.1 * config.avant_max_a
-        )
-
-        obs = observation["observation"][0][:3]
-        a = 0.127                  # AFS parameter, check the paper page(1) Figure 1: AFS mechanism
-        b = 0.495                  # AFS parameter, check the paper page(1) Figure 1: AFS mechanism
-        eps0 = 1.4049900478554351  # the angle from of the hydraulic sylinder check the paper page(1) Figure (1) 
-        eps = eps0 - obs[0]
-        k = 10 * a * b * np.sin(eps) / np.sqrt(a**2 + b**2 - 2*a*b*np.cos(eps))
-
-        env_controls = np.r_[k * self.beta_vel, self.vel_f/3]
-        
-        return env_controls, np.empty(0)
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--solver")
-    parser.add_argument("--rl", action="store_true")
     args = parser.parse_args()
 
-    if not args.rl:
-        if args.solver == "casadi_collocation":
-            solver_class = CasadiCollocationSolver
-        elif args.solver == "casadi_shooting":
-            solver_class = CasadiShootingSolver
-        elif args.solver == "acados":
-            solver_class = AcadosSolver
-        else:
-            raise ValueError(f"Unknown solver {args.solver}")
-        actor = MPCActor(solver_class=solver_class)
+    if args.solver == "casadi_collocation":
+        solver_class = CasadiCollocationSolver
+    elif args.solver == "acados":
+        solver_class = AcadosSolver
     else:
-        actor = RLActor()
+        raise ValueError(f"Unknown solver {args.solver}")
+    actor = MPCActor(solver_class=solver_class)
 
     # Initialize environment and actor
     env = AvantGoalEnv(num_envs=1, dt=0.1, time_limit_s=30, device='cpu', eval=True)
